201c1fb37ff131aa462d24b7aec0cb0630f283fb
Mik <mikvv90@yandex.ru>
2022-08-17 19:07:52 +0400

    Разбить приложение на два. Создать Docker контейнеры

---
+++ server/src/main/java/ru/practicum/shareit/requests/ItemRequestController.java
@@ -0,0 +1,48 @@
+package ru.practicum.shareit.requests;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.exceptions.IncorrectParameterException;
+import ru.practicum.shareit.requests.dto.ItemRequestDto;
+import ru.practicum.shareit.requests.dto.ItemRequestExtDto;
+
+import java.util.Collection;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/requests")
+public class ItemRequestController {
+
+    private final ItemRequestService itemRequestService;
+
+    @PostMapping
+    public ItemRequestDto create(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                 @RequestBody ItemRequestDto itemRequestDto) {
+        return itemRequestService.create(userId, itemRequestDto);
+    }
+
+    @GetMapping
+    public Collection<ItemRequestExtDto> getForUser(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        return itemRequestService.getForUser(userId);
+    }
+
+    @GetMapping("/all")
+    public Collection<ItemRequestExtDto> getAll(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                @RequestParam(defaultValue = "0") Integer from,
+                                                @RequestParam(defaultValue = "10") Integer size) {
+        if (from < 0) {
+            throw new IncorrectParameterException("from", from.toString());
+        }
+        if (size < 0) {
+            throw new IncorrectParameterException("size", size.toString());
+        }
+        return itemRequestService.getAll(userId, from, size);
+    }
+
+    @GetMapping("/{requestId}")
+    public ItemRequestExtDto getById(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                     @PathVariable Long requestId) {
+        return itemRequestService.getById(userId, requestId);
+    }
+
+}
